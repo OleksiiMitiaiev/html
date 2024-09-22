e4fe4600fcf8340eb9b27e8912715683cb8e80b6
OleksiiMitiaiev <dsst849015@gmail.com>
2024-09-22 15:24:41 +0000

    add new file index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log('Start');
+function ChekforEvenodd(num){
+    if (num % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+console.log(ChekforEvenodd(11));
+console.log("Finish")
