46ab08822ef39567fa6be5a19eec71fcfd8f7d70
OleksiiMitiaiev <dsst849015@gmail.com>
2024-10-08 16:21:23 +0000

    add new function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-console.log('Start');
-function ChekforEvenodd(num){
+/*console.log('Start');
+function ChekforEvenodd(num) {
     if (num % 2 === 0) {
         return "Even";
     } else {
@@ -7,4 +7,15 @@ function ChekforEvenodd(num){
     }
 }
 console.log(ChekforEvenodd(11));
-console.log("Finish")
+console.log("Finish");*/
+
+const a = 1;
+const b = 3;
+
+const total=add(a,b);
+console.log(total);
+
+function total(num1, num2){
+    total=num1 + num2;
+    return Result;
+}
